da8be0d45ef8f4f8acae922cef9aeffa0597c4a2
Ranel Laurente Dahil <ranellaurentedahil1401@gmail.com>
2025-09-18 20:09:58 +0800

    helllllo

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/capstonefinal/public/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <style>
+   
+   <style>
         /* Optimized Print-Specific CSS for Thermal Receipt Printer with Centered Details */
         /* These styles are specifically for printing receipts and do not affect screen responsiveness. */
         @media print {
